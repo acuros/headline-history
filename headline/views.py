4ecb13d2fe3c916015e4214493168d6ccab00470
acuros <acuroskr@gmail.com>
2013-12-26 06:16:08 +0900

    Home page implemented

--- headline/views.py
+++ headline/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from headline.models import Headline, Press, CrawlLog
 
-# Create your views here.
+def home(request):
+    headlines = Headline.objects.all().order_by('-id')
+    presses = Press.objects.all()
+    last_log = CrawlLog.objects.all().order_by('-id').first()
+    return render(request, 'home.html', dict(headlines=headlines, presses=presses, last_log=last_log))
