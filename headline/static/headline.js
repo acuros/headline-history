528c04d6e144a4ee790069e46274797f00117b59
acuros <acuroskr@gmail.com>
2013-12-27 01:39:04 +0900

    JS improved

--- headline/static/headline.js
+++ headline/static/headline.js
@@ -1,17 +1,27 @@
+var hasMoreHeadlines = true;
+
 function getMore()
 {
-  $.get('/more/' + $('.headline:last').attr('data-id'), function (headlines) {
+  if(!hasMoreHeadlines)
+      return;
+  setTimeout()
+  $.get('/more/' + $('.headline:last').attr('data-id'), function (headlines)
+  {
     headlines.forEach(function(headline)
     {
-	  var $li = $('<li></li>');
+      var $li = $('<li></li>');
       var $article = $('<article class="headline"></article>');
       $article.attr('data-id', headline.id);
       $article.append($('<h1>' + headline.pressName + '</h1>'));
       $article.append($('<a href="'+headline.link+'">'+headline.title+'</a>'));
       $article.append($('<small>('+headline.crawledTime+')</small>'));
-	  $li.append($article);
+      $li.append($article);
       $('#headline-list').append($li);
     });
+    if(headlines.length != 30)
+    {
+      hasMoreHeadlines = false;
+    }
   });
 }
 
@@ -21,12 +31,5 @@ $(function()
   {
     e.preventDefault();
     getMore();
-  });
-  $(window).scroll(function ()
-  {
-    if ($(window).scrollTop() + $(window).height() == $(document).height())
-    {
-      getMore();
-    }
   });
 });
