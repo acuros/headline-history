d73054105d18ed20b5267fcb5b4b521fb858885d
Acuros Kim <acuros@kaist.ac.kr>
2013-12-27 01:14:18 +0900

    Get more implemented

---
+++ headline/static/headline.js
@@ -0,0 +1,30 @@
+function getMore()
+{
+  $.get('/more/' + $('.headline:last').attr('data-id'), function (headlines) {
+    headlines.forEach(function(headline)
+    {
+      var $article = $('<article class="headline"></article>');
+      $article.attr('data-id', headline.id);
+      $article.append($('<h1>' + headline.pressName + '</h1>'));
+      $article.append($('<a href="'+headline.link+'">'+headline.title+'</a>'));
+      $article.append($('<small>('+headline.crawledTime+')</small>'));
+      $('.headline').append($article);
+    });
+  });
+}
+
+$(function()
+{
+  $('#get-more').click(function(e)
+  {
+    e.preventDefault();
+    getMore();
+  });
+  $(window).scroll(function ()
+  {
+    if ($(window).scrollTop() + $(window).height() == $(document).height())
+    {
+      getMore();
+    }
+  });
+});
--- headline/views.py
+++ headline/views.py
@@ -1,8 +1,28 @@
+import ujson as json
+
+from datetime import datetime
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from headline.models import Headline, Press, CrawlLog
 
+
 def home(request):
-    headlines = Headline.objects.all().order_by('-id')
+    headlines = Headline.objects.all().order_by('-id')[:30]
     presses = Press.objects.all()
     last_log = CrawlLog.objects.all().order_by('-id').first()
-    return render(request, 'home.html', dict(headlines=headlines, presses=presses, last_log=last_log))
+    return render(request, 'home.html', dict(headlines=headlines,
+                                             presses=presses,
+                                             last_log=last_log))
+
+
+def more(request, last_headline_id):
+    headlines = Headline.objects.filter(id__gte=last_headline_id).order_by('-id')[:30]
+    headlines = [dict(id=headline.id,
+                      pressName=headline.press.name,
+                      link=headline.link,
+                      title=headline.title,
+                      crawledTime=datetime.strftime(headline.crawled_time,
+                                                    '%y-%m-%d %H:%M'))
+                 for headline in headlines]
+    return HttpResponse(json.dumps(headlines), mimetype='application/json')
