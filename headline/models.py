d29cd3fbf24d57f9564c060df33a28ff8e544b04
acuros <acuros@kaist.ac.kr>
2013-12-28 02:14:26 +0900

    Use relative_time on infinite scroll too

--- headline/models.py
+++ headline/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from utils import to_relative_time
 
 
 class Press(models.Model):
@@ -10,6 +11,14 @@ class Headline(models.Model):
     title = models.TextField()
     link = models.TextField()
     crawled_time = models.DateTimeField(auto_now_add=True)
+
+    def to_dict(self):
+        return dict(id=self.id,
+                      press_name=self.press.name,
+                      link=self.link,
+                      title=self.title,
+                      crawled_time=to_relative_time(self.crawled_time)
+                    )
 
 
 class CrawlLog(models.Model):
