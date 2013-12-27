202f939d2665c32aedb05f91eff205224aabeec9
theeluwin <theeluwin@gmail.com>
2013-12-27 21:40:49 +0900

    adding more headlines changed: handled by handlebar

--- headline/static/headline.js
+++ headline/static/headline.js
@@ -3,32 +3,22 @@ var hasMoreHeadlines = true;
 function getMore()
 {
   if(!hasMoreHeadlines)
-      return;
-  setTimeout()
-  $.get('/more/' + $('.headline:last').attr('data-id'), function (headlines)
+    return;
+  $.get('/more/' + $('.headline:last').attr('data-id'), function(headlines)
   {
-    headlines.forEach(function(headline)
-    {
-      var $li = $('<li class="list-group-item"></li>');
-      var $article = $('<article class="headline"></article>');
-      $article.attr('data-id', headline.id);
-	  var $h4 = $('<h4 class="list-group-item-heading"></h4>');
-	  $h4.append('<b>' + headline.pressName + '</b>');
-	  $h4.append('<span class="badge pull-right">'+headline.crawledTime+')</span>');
-      $article.append($h4);
-	  $article.append($('<p class="list-group-item-text"><a href="'+headline.link+'">'+headline.title+'</a></p>'));
-      $li.append($article);
-      $('#headline-list').append($li);
-    });
+    var data = { 'headlines': headlines };
+    var template = Handlebars.compile($("#headline-template").html());
+    var $headlines = $(template(data));
+    $("#headline-list").append($headlines);
     if(headlines.length != 50)
     {
       hasMoreHeadlines = false;
-	  $('#get-more').remove();
+	    $('#get-more').remove();
     }
   });
 }
 
-$(function()
+$(document).ready(function()
 {
   $('#get-more').click(function(e)
   {
