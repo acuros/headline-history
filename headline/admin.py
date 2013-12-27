9c8e85d07e3d54c06b1d5d5183adbd8343b536ee
theeluwin <theeluwin@gmail.com>
2013-12-28 01:18:19 +0900

    made django admin page fully functional

--- headline/admin.py
+++ headline/admin.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from headline.models import *
 
-# Register your models here.
+
+class HeadlineInline(admin.TabularInline):
+    model = Headline
+    extra = 0
+
+
+class PressAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+    inlines = (HeadlineInline, )
+
+
+class HeadlineAdmin(admin.ModelAdmin):
+    list_display = ('press', 'title', 'crawled_time', )
+    list_filter = ('crawled_time', )
+    search_fields = ('press__name', 'title', )
+
+
+class CrawlLogAdmin(admin.ModelAdmin):
+    list_display = ('crawled_time', )
+
+
+admin.site.register(Press, PressAdmin)
+admin.site.register(Headline, HeadlineAdmin)
+admin.site.register(CrawlLog, CrawlLogAdmin)
