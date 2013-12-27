096ff07c148fb44befdbcbf83ff01cb24400ee9d
theeluwin <theeluwin@gmail.com>
2013-12-28 01:17:59 +0900

    added meta data, and unicode function

--- headline/models.py
+++ headline/models.py
@@ -2,10 +2,26 @@ from django.db import models
 
 
 class Press(models.Model):
+
+    class Meta:
+        verbose_name = u'Press'
+        verbose_name_plural = u'Presses'
+        
+    def __unicode__(self):
+        return u'%s' % self.name
+    
     name = models.CharField(max_length=50)
 
 
 class Headline(models.Model):
+
+    class Meta:
+        verbose_name = u'Headline'
+        verbose_name_plural = u'Headlines'
+    
+    def __unicode__(self):
+        return u'<%s> %s' % (self.press.name, self.title)
+        
     press = models.ForeignKey(Press)
     title = models.TextField()
     link = models.TextField()
@@ -13,5 +29,13 @@ class Headline(models.Model):
 
 
 class CrawlLog(models.Model):
+
+    class Meta:
+        verbose_name = u'CrawlLog'
+        verbose_name_plural = u'CrawlLogs'
+    
+    def __unicode__(self):
+        return u'crawled at %s' % self.crawled_time.strftime(u'%m/%d %H:%M')
+    
     updated_presses = models.ManyToManyField(Press)
     crawled_time = models.DateTimeField(auto_now_add=True)
