1d86248319519a94f6235b3fc5511d04ca38c49f
acuros <acuros@kaist.ac.kr>
2013-12-29 02:21:35 +0900

    Emphasize query in result

--- headline/templatetags/filters.py
+++ headline/templatetags/filters.py
@@ -1,4 +1,7 @@
+import re
+
 from django import template
+from django.utils.safestring import mark_safe
 from headline.utils import to_relative_time
 
 register = template.Library()
@@ -6,3 +9,11 @@ register = template.Library()
 @register.filter(name='relative_time')
 def relative_time(value):
     return to_relative_time(value)
+
+@register.filter(name='emphasize')
+def emphasize(value, query):
+    pattern = re.escape(query)
+    repl = ur'<span class="red">\g<0></span>'
+    emphasized = re.sub(pattern, repl, value, flags=re.IGNORECASE)
+    return mark_safe(emphasized)
+
