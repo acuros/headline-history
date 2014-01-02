c30a547f31d5a7fc34c62b79d88d406291dc27e7
Hyunjun Kim <kim@hyunjun.kr>
2014-01-02 13:59:00 +0900

    Two empty lines are expected between global definitions.

--- headline/templatetags/filters.py
+++ headline/templatetags/filters.py
@@ -6,9 +6,11 @@ from headline.utils import to_relative_time
 
 register = template.Library()
 
+
 @register.filter(name='relative_time')
 def relative_time(value):
     return to_relative_time(value)
+
 
 @register.filter(name='emphasize')
 def emphasize(value, query):
@@ -16,4 +18,3 @@ def emphasize(value, query):
     repl = ur'<span class="red">\g<0></span>'
     emphasized = re.sub(pattern, repl, value, flags=re.IGNORECASE)
     return mark_safe(emphasized)
-
