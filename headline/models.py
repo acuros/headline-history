0dad015561c338df70908a027e3993cb01f2a5b1
Hyunjun Kim <kim@hyunjun.kr>
2014-01-02 13:57:37 +0900

    Remove whitespaces in empty lines.

--- headline/models.py
+++ headline/models.py
@@ -8,10 +8,10 @@ class Press(models.Model):
     class Meta:
         verbose_name = u'Press'
         verbose_name_plural = u'Presses'
-        
+
     def __unicode__(self):
         return u'%s' % self.name
-    
+
     name = models.CharField(max_length=50)
 
 
@@ -20,10 +20,10 @@ class Headline(models.Model):
     class Meta:
         verbose_name = u'Headline'
         verbose_name_plural = u'Headlines'
-    
+
     def __unicode__(self):
         return u'<%s> %s' % (self.press.name, self.title)
-        
+
     press = models.ForeignKey(Press)
     title = models.TextField()
     link = models.TextField()
@@ -45,9 +45,9 @@ class CrawlLog(models.Model):
     class Meta:
         verbose_name = u'CrawlLog'
         verbose_name_plural = u'CrawlLogs'
-    
+
     def __unicode__(self):
         return u'crawled at %s' % self.crawled_time.strftime(u'%m/%d %H:%M')
-    
+
     updated_presses = models.ManyToManyField(Press)
     crawled_time = models.DateTimeField(auto_now_add=True)
